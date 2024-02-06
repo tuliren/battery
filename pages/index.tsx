b0688c0c8359759ac662d5a26f5abb73bc20dd9e
Liren Tu <liren.tu@benchling.com>
2024-02-06 14:25:24 -0800

    Simplify layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Paper, Progress, useMantineTheme } from '@mantine/core';
+import { Box, Center, Overlay, Paper, Progress, useMantineTheme } from '@mantine/core';
 import { IconBolt } from '@tabler/icons-react';
 import type { NextPage } from 'next';
 
@@ -6,43 +6,39 @@ const Home: NextPage = () => {
   const theme = useMantineTheme();
 
   return (
-    <div
+    <Paper
       style={{
+        position: 'relative',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         height: '90vh',
       }}
     >
-      <Paper
+      <Box
         style={{
-          boxShadow: theme.shadows.xl,
-          position: 'relative',
+          // boxShadow: `5px 5px 10px ${theme.colors.dark[2]}`,
+          border: `8px solid ${theme.colors.dark[4]}`,
+          width: 300,
+          height: 100,
+          borderRadius: '15px',
+          backgroundColor: '#f3f3f3',
+          zIndex: 10,
+        }}
+      />
+
+      <Box
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 100,
         }}
       >
-        <Box
-          style={{
-            boxShadow: `5px 5px 10px ${theme.colors.dark[2]}`,
-            border: `10px solid ${theme.colors.dark[4]}`,
-            width: 300,
-            height: 100,
-            borderRadius: '15px',
-            backgroundColor: '#f3f3f3',
-          }}
-        />
-
-        <Box
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-          }}
-        >
-          <IconBolt size={55} stroke={1.2} color={theme.colors.dark[4]} />
-        </Box>
-      </Paper>
-    </div>
+        <IconBolt size={55} stroke={1.4} color={theme.colors.dark[4]} />
+      </Box>
+    </Paper>
   );
 };
 
