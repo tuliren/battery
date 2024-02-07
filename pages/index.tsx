9f357d1371c3a7c1c1e3634c58944f94e7ecb8e8
Liren Tu <tuliren@gmail.com>
2024-02-06 17:02:49 -0800

    Add control and color

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import {
   Card,
   Center,
   Grid,
-  Paper,
   Progress,
+  SegmentedControl,
   Slider,
   Stack,
   useMantineTheme,
@@ -16,8 +16,12 @@ import { useState } from 'react';
 const percentageMargin = 8;
 
 const Home: NextPage = () => {
+  const [chargingStatus, setChargingStatus] = useState<'on' | 'off'>('on');
   const [percentage, setPercentage] = useState(85);
   const theme = useMantineTheme();
+  const lowBatteryColor = theme.colors.red[4];
+  const mediumBatteryColor = theme.colors.blue[4];
+  const highBatteryColor = theme.colors.green[4];
 
   return (
     <Center>
@@ -44,6 +48,15 @@ const Home: NextPage = () => {
             <Progress
               size="xl"
               value={percentage}
+              color={
+                chargingStatus === 'on'
+                  ? highBatteryColor
+                  : percentage < 20
+                  ? lowBatteryColor
+                  : percentage < 80
+                  ? mediumBatteryColor
+                  : highBatteryColor
+              }
               style={{
                 position: 'absolute',
                 bottom: percentageMargin, // Align to the bottom of the parent Box with margin
@@ -57,35 +70,53 @@ const Home: NextPage = () => {
             />
           </Box>
 
-          <Box
-            style={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              zIndex: 20,
-            }}
-          >
-            <IconBolt size={55} stroke={1.4} color={theme.colors.dark[4]} />
-          </Box>
+          {chargingStatus === 'on' && (
+            <Box
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                zIndex: 20,
+              }}
+            >
+              <IconBolt size={55} stroke={1.4} color={theme.colors.dark[4]} />
+            </Box>
+          )}
         </Card.Section>
 
-        <Grid>
-          <Grid.Col span={4}>Percentage</Grid.Col>
-          <Grid.Col span={8}>
-            <Slider
-              min={0}
-              max={100}
-              value={percentage}
-              onChange={setPercentage}
-              marks={[
-                { value: 20, label: '20%' },
-                { value: 50, label: '50%' },
-                { value: 80, label: '80%' },
-              ]}
-            />
-          </Grid.Col>
-        </Grid>
+        <Stack gap="xl">
+          <Grid>
+            <Grid.Col span={4}>Charging</Grid.Col>
+            <Grid.Col span={8}>
+              <SegmentedControl
+                value={chargingStatus}
+                onChange={(value) => setChargingStatus(value as 'on' | 'off')}
+                data={[
+                  { label: 'On', value: 'on' },
+                  { label: 'Off', value: 'off' },
+                ]}
+              />
+            </Grid.Col>
+          </Grid>
+
+          <Grid>
+            <Grid.Col span={4}>Percentage</Grid.Col>
+            <Grid.Col span={8}>
+              <Slider
+                min={1}
+                max={100}
+                value={percentage}
+                onChange={setPercentage}
+                marks={[
+                  { value: 20, label: '20%' },
+                  { value: 50, label: '50%' },
+                  { value: 80, label: '80%' },
+                ]}
+              />
+            </Grid.Col>
+          </Grid>
+        </Stack>
       </Card>
     </Center>
   );
